72fa339e4d18c49682cbaa4b5de809efff712a73
Bjartur Thorhalls <bjartur@gestur-024.ru.is>
2019-09-05 14:36:03 +0000

    Finished the code

--- sequence.py
+++ sequence.py
@@ -10,6 +10,11 @@ nr_of_sequences = int(input("How many numbers do you want: "))
 first_nr = 1
 second_nr = 2
 third_nr =  3
+
 for x in range(1, nr_of_sequences+1):
-    nr = first_nr+second_nr+third_nr
-    print(nr)
+    if x <= 3:
+        print(x)
+    else:
+        nr = first_nr+second_nr+third_nr
+        first_nr, second_nr, third_nr = second_nr, third_nr, nr
+        print(nr)
