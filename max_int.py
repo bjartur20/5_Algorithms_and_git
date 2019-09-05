f1d325990273b748621dacc6d4df4e7aba0e4d9a
Bjartur Thorhalls <bjartur@gestur-024.ru.is>
2019-09-05 13:47:13 +0000

    Added a while loop which runs until the input is less than zero and stores the largest number

---
+++ max_int.py
@@ -0,0 +1,13 @@
+'''
+1. Takes some input (positive int) from the user.
+2. Stores the largest number which the user types in.
+3. Stops asking for input when the user inputs a negative number.
+4. Prints the largest number.
+'''
+
+#Bjartur Thorhallsson
+
+number = int(input("Input a number: "))
+while number >= 0:
+    largest_number = number
+    number = int(input("Input a number: "))
